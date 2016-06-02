1a7234b1f2b449263d423635a5422d7a81fec33f
jorda <jorda@192.168.1.25>
2016-06-02 15:20:42 -0400

    main methods

---
+++ client/main/client/ClientStartUp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * @author jorda
+ *
+ */
+public class ClientStartUp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ server/main/server/ServerStartUp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author jorda
+ *
+ */
+public class ServerStartUp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
