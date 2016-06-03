6ef38bdb180c5f80a53d53af0c7e47ecd5152a71
anthonyyu <anthonyyu@192.168.1.26>
2016-06-03 16:07:55 -0400

    7

--- Project1/ClientComp/main/Client.java
+++ Project1/ClientComp/main/Client.java
@@ -2,7 +2,7 @@
  * LAST REVISION: 6/3/2016
  */
 
-package main;
+package client;
 
 import java.io.*;
 import java.net.*;
@@ -26,12 +26,18 @@ public class Client implements ClientConnect {
 		// LOGGING CODE
 		if(logChoice.equalsIgnoreCase("yes")) {
 			if(logMethod.equalsIgnoreCase("txt")) {
-				//Text file logging
-				File file = new File("ClientLog.txt");
-				FileOutputStream fos = new FileOutputStream(file);
-				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-				if(debug) System.out.println("DEBUG: Sucessfully created log output text file.");
-				logElements[0] = "Client.java: Sucessfully created log output text file";
+				try {
+					//Text file logging
+					File file = new File("ClientLog.txt");
+					FileOutputStream fos = new FileOutputStream(file);
+					BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+					if(debug) System.out.println("DEBUG: Sucessfully created log output text file.");
+					logElements[0] = "Client.java: Sucessfully created log output text file";
+				} catch(FileNotFoundException fnf) {
+					System.out.println("File unable to be found.");
+				} catch(IOException ioe) {
+					System.out.println("File unable to be written to.");
+				}
 			} else {
 				//Command line logging
 				if(debug) System.out.println("DEBUG: Command line logging chosen.");
@@ -47,6 +53,8 @@ public class Client implements ClientConnect {
 			Thread newClientThread = new Thread(new ClientThread(hostName, portNumber, s));
 			if(debug) System.out.println("DEBUG: Created a new client thread."); 
 			logElements[2] = "Client.java: Sucessfully created a new client thread";
+		} catch(SocketException se) {
+			System.out.println("Error with creating socket.");
 		} catch(UnknownHostException uhe) {
 			System.out.println("Error connecting to " + hostName);
 			System.exit(1);
@@ -73,3 +81,86 @@ public class Client implements ClientConnect {
 		this.logMethod = logMethod;
 	}
 }
+
+
+
+
+
+
+//	//Variable declarations
+//	private static boolean debug = true;
+//	
+//	//private static String hostName;		//IP address of Host
+//	//private static int portNumber;		//Port number to connect to
+//	//private static String logChoice;	//Logging? (yes or no)
+//	//private static String logMethod;		//Log method (Text file or Command line)
+//	private static String inputFromClient;		//String input from client
+//	private static String responseFromServer;	//String output from server
+//	private String[] logArray = new String[3];	//Array that contains all logging information
+//	private static DataInputStream disClient;	//Input from Client
+//	private static DataInputStream disServer;	//Input from Server
+//	
+//	public static void main(String[] args) throws IOException {
+//		
+//		if(debug) System.out.println("Debug: ClientStartUp started.");
+//		
+//		hostName = args[0];
+//		portNumber = Integer.parseInt(args[1]);
+//		logChoice = args[2];
+//		logMethod = args[3];
+//		
+//		// Look for 4 command line arguments: host, port #, logging option, logging method
+//		if (args.length != 4) {
+//			System.out.println("Error. Command line arguments needed: Host, Port number, Logging (yes or no), Logging method (command line[cmd] or text file[txt])");
+//		}
+//		
+//		/* LOGGING CODE
+//		if(logChoice.equalsIgnoreCase("yes")) {
+//			if(logMethod.equalsIgnoreCase("txt")) {
+//				//Text file logging
+//				File file = new File("ClientLog.txt");
+//				FileOutputStream fos = new FileOutputStream(file);
+//				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+//			} else {
+//				//Command line logging
+//				//Code for cmd line logging
+//			}
+//		}
+//		*/
+//		
+//		try {
+//			//Establish connection with server and open up communication
+//			Socket s = new Socket(hostName, portNumber);
+//			if(debug) System.out.println("Debug: established connection with server at: " + hostName + portNumber);
+//			//logArray[0] = "Connection to host established";
+//					
+//			//DataOutputStream dosServer = new DataOutputStream(s.getOutputStream());	//Output to server
+//			disServer = new DataInputStream(s.getInputStream());
+//			disClient = new DataInputStream(System.in);
+//		
+//			responseFromServer = disServer.readUTF();
+//			inputFromClient = disClient.readUTF();
+//			
+//			while(responseFromServer != null) {
+//				System.out.print("Server: " + responseFromServer);
+//				//logArray[1] = "String recieved from server: " + responseFromServer;
+//				
+//				if(responseFromServer.equalsIgnoreCase("end")) {
+//					if(debug) System.out.println("Debug: server ended connection");
+//					//logArray[3] = "Server ended connection.";
+//					break;
+//				}
+//				
+//				if(inputFromClient != null) {
+//					System.out.println("Client: " + inputFromClient);
+//					//logArray[2] = "String recieved from client: " + inputFromClient;
+//				}
+//			}
+//		} catch(UnknownHostException uhe) {
+//			System.out.println("Error connecting to " + hostName);
+//			System.exit(1);
+//		}
+//		
+//	}
+//
+//}
